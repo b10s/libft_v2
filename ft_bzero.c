8525dcfac42c37023889da03e4d45d6e146998a2
Andrei Enshin <aenshin@c1r23s1.42tokyo.jp>
2023-05-25 23:26:27 +0900

    add bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/21 00:29:08 by aenshin           #+#    #+#             */
-/*   Updated: 2023/05/25 23:13:26 by aenshin          ###   ########.fr       */
+/*   Created: 2023/05/25 23:12:48 by aenshin           #+#    #+#             */
+/*   Updated: 2023/05/25 23:26:08 by aenshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <stddef.h>
 
-# include <stddef.h>
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-
-#endif
+void
+	ft_bzero(void *s, size_t n) {
+	while (n > 0)
+	{
+		*(char *)s = '\0';
+		s++;
+		n--;
+	}
+}
