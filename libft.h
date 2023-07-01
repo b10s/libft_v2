54bba894e04a94a42e075358a96659171b2b4d78
Andrei Enshin <aenshin@c1r7s2.42tokyo.jp>
2023-07-01 16:23:02 +0900

    ft_strlcpy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 00:29:08 by aenshin           #+#    #+#             */
-/*   Updated: 2023/05/27 23:38:15 by aenshin          ###   ########.fr       */
+/*   Updated: 2023/07/01 16:22:33 by aenshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
