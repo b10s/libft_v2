b5f7fbbced455f6eccf05b1b01796eeb44b2c018
Andrei Enshin <aenshin@c1r7s5.42tokyo.jp>
2023-07-03 18:34:24 +0900

    toupper, tolower

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 00:29:08 by aenshin           #+#    #+#             */
-/*   Updated: 2023/07/01 18:28:14 by aenshin          ###   ########.fr       */
+/*   Updated: 2023/07/03 18:34:02 by aenshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
