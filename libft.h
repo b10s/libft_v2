ca0a2f80b57922d08ab1ddd1e0d2280641c4bb5d
Andrei Enshin <aenshin@c1r9s9.42tokyo.jp>
2023-07-18 22:02:33 +0900

    lstnew

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 00:29:08 by aenshin           #+#    #+#             */
-/*   Updated: 2023/07/17 19:38:30 by aenshin          ###   ########.fr       */
+/*   Updated: 2023/07/18 21:53:09 by aenshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 # define LIBFT_H
 
 # include <stddef.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
