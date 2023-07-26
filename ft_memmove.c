0bb1eac8d58fb41994528adf82a6fd2df62b8d9f
Andrei Enshin <aenshin@c1r7s1.42tokyo.jp>
2023-07-26 21:45:33 +0900

    add comment for UB for memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,12 +6,15 @@
 /*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 23:35:01 by aenshin           #+#    #+#             */
-/*   Updated: 2023/05/27 23:47:13 by aenshin          ###   ########.fr       */
+/*   Updated: 2023/07/24 22:26:34 by aenshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
+// C ISO/IEC 9899:TC3
+// 7.1.12 (1): null pointer leads to UB
+// 7.21.1 (2): do nothing with zero size
 void *
 	ft_memmove(void *dst, const void *src, size_t len) {
 	void	*tmp;
