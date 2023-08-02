ae3d4a1a48242b7b391c426b601215dfc734b0bb
Andrei Enshin <aenshin@c1r11s5.42tokyo.jp>
2023-08-03 03:31:53 +0900

    fix lsmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 23:22:28 by aenshin           #+#    #+#             */
-/*   Updated: 2023/07/24 21:54:20 by aenshin          ###   ########.fr       */
+/*   Updated: 2023/08/03 03:31:02 by aenshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ t_list *
 	ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *)) {
 	t_list	*res;
 	t_list	*new;
-	t_list	*tmp;
 
-	if (lst == NULL)
+	res = NULL;
+	if (lst == NULL || f == NULL)
 		return (NULL);
-	tmp = lst;
 	while (lst != NULL)
 	{
 		new = ft_lstnew(f(lst->content));
@@ -30,7 +29,7 @@ t_list *
 			ft_lstclear(&res, del);
 			return (NULL);
 		}
-		ft_lstadd_front(&res, new);
+		ft_lstadd_back(&res, new);
 		lst = lst->next;
 	}
 	return (res);
