08f6c5ae956c37e5bfcb2bae7c23ef5f7a33dd87
Andrei Enshin <aenshin@c1r11s5.42tokyo.jp>
2023-08-03 02:36:03 +0900

    check overflow in calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,25 +6,26 @@
 /*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 20:57:35 by aenshin           #+#    #+#             */
-/*   Updated: 2023/07/08 21:03:25 by aenshin          ###   ########.fr       */
+/*   Updated: 2023/08/03 02:32:20 by aenshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <limits.h>
+#include "./libft.h"
 
 void *
 	ft_calloc(size_t count, size_t size) {
 	void	*res;
-	void	*tmp;
-	size_t	i;
+	size_t	len;
 
-	i = 0;
-	res = malloc(count * size);
+	if (count != 0 && (SIZE_MAX / count) < size)
+		return (NULL);
+	len = count * size;
+	res = malloc(len);
 	if (res == NULL)
 		return (res);
-	tmp = res;
-	while (i++ < count * size)
-		*(char *)tmp++ = '\0';
+	ft_bzero(res, len);
 	return (res);
 }
